7d4365d8068654665a596abae1bf92f1bd6e8520
Abdoul-Hakim AFRAITANE <abdoul.hakim.ext@eulerhermes.com>
2023-09-15 23:55:51 +0200

    fix: remove columns on small screens

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -6,8 +6,8 @@ import Instagram from "../../public/assets/Icon/instagram.svg";
 const Footer = () => {
   return (
     <div className="bg-white-300 pt-44 pb-24 mt-60">
-      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
-        <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 flex flex-col items-start sm:col-start-9 sm:col-end-13">
+      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid  sm:grid-rows-1 grid-flow-row sm:grid-flow-col sm:grid-cols-12 gap-4">
+        <div className="row-span-2 sm:col-span-4  flex flex-col items-start sm:col-start-9 sm:col-end-13">
         <Image
                   src="/assets/Kartalys.png"
                   alt="logo-kartalys"
@@ -18,7 +18,7 @@ const Footer = () => {
           <p className="mb-4">
             <strong className="font-medium">Kartalys</strong> est une société de prestation de service informatique offrant de multiples solutions.
           </p>
-          <div className="flex w-full mt-2 mb-8 -mx-2">
+          <div className="flex w-full mt-2 mb-1 -mx-2">
             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
               <Facebook className="h-6 w-6" />
             </div>
@@ -31,19 +31,6 @@ const Footer = () => {
           </div>
           <p className="text-gray-400">©{new Date().getFullYear()} - Kartalys</p>
         </div>
-        
-        
-        {/* <div className="row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Earn Money</p>
-          <ul className="text-black-500">
-            <li className="my-2 hover:text-red-700 cursor-pointer transition-all">
-              Affiliate{" "}
-            </li>
-            <li className="my-2 hover:text-red-700 cursor-pointer transition-all">
-              Become Partner{" "}
-            </li>
-          </ul>
-        </div> */}
       </div>
     </div>
   );
