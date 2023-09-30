34e9fb1258e454fc68f7bbd5116aaad76f355143
Abdoul-Hakim AFRAITANE <abdoul.hakim.ext@eulerhermes.com>
2023-09-30 18:52:51 +0200

    fix: remove header padding

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
           (scrollActive ? " shadow-md pt-0" : " pt-4")
         }
       >
-        <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
+        <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col">
           <div className="col-start-1 col-end-2 flex items-center">
           <Image
                   src="/assets/Kartalys.png"
